fe57aa1717edfac8052e4feaea4778b42c5b3acc
Zeninn <113045813+Zeninn01@users.noreply.github.com>
2025-07-17 22:21:11 -0300

    app.js

---
+++ JavaScript/Praticas/Desafio 2/app.js
@@ -0,0 +1,47 @@
+// let nome = prompt("Qual é o seu nome?")
+// let mensagem = bemVindo()
+// console.log(mensagem)
+
+// function bemVindo() {
+//    return console.log(`Olá, ${nome}! Bem-vindo ao nosso site!`)
+// }
+
+// let numero = prompt(`Digite um número:`)
+// let mensagem = multiplicarNumero()
+
+// function multiplicarNumero() {
+//     let resultado = parseInt(numero) * 2
+//     return console.log(`O dobro de ${numero} é ${resultado}.`)
+// }
+
+// let numero1 = prompt("Digite o primeiro número:")
+// let numero2 = prompt("Digite o segundo número:")
+// let numero3 = prompt("Digite o terceiro número:")
+// let mensagem = calcularMedia()
+
+// function calcularMedia() {
+//     let media = (parseFloat(numero1) + parseFloat(numero2) + parseFloat(numero3)) / 3
+//     return console.log(`A média dos números ${numero1}, ${numero2} e ${numero3} é ${media}.`)
+// }
+
+// let numero1 = prompt("Digite o primeiro número:")
+// let numero2 = prompt("Digite o segundo número:")
+// let maiorNumero = 0
+// let mensagem = mostrarMaiorNumero()
+
+// function mostrarMaiorNumero() {
+//     if (parseInt(numero1) > parseInt(numero2)) {
+//         maiorNumero = parseInt(numero1)
+//     }else {
+//         maiorNumero = parseInt(numero2)
+//     }
+//     return console.log(`O maior número entre ${numero1} e ${numero2} é ${maiorNumero}.`)
+// }
+
+// let numero = prompt("Digite um número:")
+// let mensagem = seMultiplica()
+
+// function seMultiplica() {
+//     let resultado = parseInt(numero) * parseInt(numero)
+//     return console.log(`O resultado da multiplicação de ${numero} por ele mesmo é ${resultado}.`)
+// }
